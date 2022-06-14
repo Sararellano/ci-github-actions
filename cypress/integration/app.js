b368ed01814004ed43f2c3d401a4a7294d69a6e3
Sararellano <sara.arellano@selectra.info>
2022-06-14 15:42:06 +0200

    feat(cypress): change cypress config

--- cypress/integration/app.js
+++ cypress/integration/app.js
@@ -3,7 +3,7 @@
 // Cypress test e2e -> test if footer string is Pokémon and Pokémon character names are trademarks of Nintendo and if there's Pikachu
 
 // Los test e2e son bastante mas lentos, por eso es mejor hacer los de integración
-// lo bueno es que te hace hasta un video (lo configuras en las setting de cypress en cypress.json) -> https://docs.cypress.io/guides/references/configuration#Screenshots
+// lo bueno es que te hace hasta un video (lo configuras en las setting de cypress en cypress.config.js) -> https://docs.cypress.io/guides/references/configuration#Screenshots
 describe('Pokedex', function() {
   it('frontpage can be opened', function () {
     cy.visit('http://localhost:5000')
